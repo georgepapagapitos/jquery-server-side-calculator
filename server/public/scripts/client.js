5fda8ac1093e2fc1f7ec4c8b86e3833d25fa4832
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-05 18:36:30 -0600

    working on client side + formatted html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,26 @@
 $(document).ready(onReady);
 
-function onReady() {}
+function onReady() {
+  $(document).on('submit', '#submit-button', onSubmit);
+}
+
+function onSubmit() {
+  event.preventDefault();
+  let newCalculation = {
+    firstNumber: $('#first-number').val(),
+    secondNumber: $('#second-number').val(),
+  };
+
+  $.ajax({
+    url: '/calculator',
+    method: 'POST',
+    data: { calculation_to_add: newCalculation },
+  })
+    .then(function (response) {
+      console.log('in POST response', response);
+      // function to execute
+    })
+    .catch(function (error) {
+      console.log('error', error);
+    });
+}
